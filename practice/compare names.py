31c58e85777d229ce133c5aeb0a0824570b03630
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-10-07 16:14:02 +0100

    handle errorr input

--- practice/compare names.py
+++ practice/compare names.py
@@ -3,26 +3,31 @@ names = []
 ordered = True
 
 while True:
-    x = int(input("How many names you want to compare?\nType in Here: "))
-    if x == 1 or x == 0:
-        print("You have to enter 2 names or more!\nLet's try again.\n.\n..\n...")
+    try:
+        x = int(input("How many names you want to compare?\nType in Here: "))
+        if x < 2:
+            print("You have to enter 2 names or more!\nLet's try again.\n.\n..\n...")
 
-    else:
-        while i < x:
-            name = str(input(f"Enter nom{i + 1}: "))
-            name = name.upper()
-            names.append(name)
-            i += 1
+        elif x >= 2:
+            while i < x:
+                name = str(input(f"Enter nom{i + 1}: "))
+                name = name.upper()
+                names.append(name)
+                i += 1
 
-        print(names)
-        while j < (x - 1):
-            if names[j] > names[j + 1]:
-                ordered = False  # If names are not ordered, set the flag to False
-            j += 1
+            while j < (x - 1):
+                if names[j] > names[j + 1]:
+                    ordered = False  # If names are not ordered, set the flag to False
+                j += 1
 
-        if ordered:
-            print("\n==> Names are ordered alphabetically.")
-            break
-        else:
-            print("\n==>Names are not ordered alphabetically.")
-            break
+            if ordered:
+                print("\n==> Names are ordered alphabetically.")
+                names.clear()
+                print(names)
+                break
+            else:
+                print("\n==>Names are not ordered alphabetically.")
+                break
+
+    except ValueError:
+        print("Invalid input! Please enter a valid number for the number of names.")
